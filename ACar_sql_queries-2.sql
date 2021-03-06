1c9ad625f2538a88e5c982a79fbf567b23a074d6
Marina Volkova <sungria@gmail.com>
2021-03-06 11:07:27 +0100

    fixed

---
+++ ACar_sql_queries-2.sql
@@ -0,0 +1,91 @@
+﻿SELECT r.vehicleid, r.ownerid,
+           r.registrationnum,
+           r."From",
+           r."To",
+           --o.name,
+           --v.licence
+    FROM registration AS r
+    JOIN vehicle AS v ON vehicleid = v.id  
+    JOIN owner AS o ON ownerid = o.id WHERE v.licence = 'BR512' AND o.name = 'Sungria';
+
+ALTER TABLE vehicle RENAME licence TO vin;
+ALTER TABLE vehicle ALTER COLUMN vin SET NOT NULL;
+ALTER TABLE vehicle ADD CONSTRAINT constraint_unique UNIQUE (vin);
+
+ALTER TABLE registration ALTER COLUMN registrationnum SET NOT NULL;
+
+SELECT v.brand_id,
+       v.model_id,
+       r.registrationnum,
+       --o.name,
+       r."From",
+       r."To"
+FROM registration as r
+JOIN vehicle AS v ON vehicleid = v.id 
+JOIN owner as o ON ownerid = o.id
+
+SELECT brand, model, year 
+FROM 
+   (SELECT v.vin,
+       b.name as brand,
+       m.name as model,
+       m.year
+   FROM brand as b
+   JOIN model as m ON b.id = m.brand_id
+   JOIN vehicle AS v ON v.model_id = m.id) AS sub
+ WHERE vin = 'VV404';
+
+SELECT name, regnum, "From", "To"
+FROM (
+    SELECT o.name, 
+       r.registrationnum AS regnum,
+       r."From",
+       r."To",
+       v.vin
+    FROM registration AS r
+    JOIN owner AS o ON o.id = r.ownerid
+    JOIN vehicle AS v ON v.id = r.vehicleid) AS sub
+ WHERE vin = 'AA123'
+
+EXPLAIN
+ WITH cte_reg AS (
+    SELECT o.name, 
+       r.registrationnum AS regnum,
+       r."From",
+       r."To",
+       v.vin
+    FROM registration AS r
+    JOIN owner AS o ON o.id = r.ownerid
+    JOIN vehicle AS v ON v.id = r.vehicleid)
+    WHERE r."To" IS NULL
+
+SELECT name, regnum, "From", "To"
+FROM cte_reg
+WHERE vin = 'AA123' AND "To" IS NULL;
+
+UPDATE registration r
+SET "To" = now()
+FROM vehicle v
+WHERE r.vehicleid = v.id AND v.vin = 'AA123' AND r."To" IS NULL;
+
+ALTER TABLE registration DROP CONSTRAINT registration_pkey;
+ALTER TABLE registration ADD PRIMARY KEY (registrationnum);
+
+EXPLAIN SELECT b.name, m.name, oreg.name, oreg."From"
+FROM (
+   SELECT r.vehicleid, v.vin, o.name, r."From",
+          v.brand_id, v.model_id 
+   FROM registration r, vehicle v, owner o
+   WHERE r."To" IS NULL
+     AND r.vehicleid = v.id
+     AND r.ownerid = o.id
+     AND v.vin = 'AA123') AS oreg
+ JOIN brand b ON b.id = oreg.brand_id
+ JOIN model m ON m.id = oreg.model_id;
+
+ALTER TABLE registration DROP CONSTRAINT registration_pkey;
+ALTER TABLE registration ADD PRIMARY KEY (vehicleid, ownerid, "From");
+ 
+
+
+   
--- update_registration.py
+++ update_registration.py
@@ -11,12 +11,20 @@ steps:
     UPDATE registration WHERE 'to' IS NULL
     INSERT .... COMMIT;
     
-@author: sungria
+@author: sungria\
+    
+    TODO:
+        - add timers on inputs, if it more that 2 minutes, close connection
+        - add user id
+        
 """
 
 import psycopg2
+import time
 
+# custom
 import db_connect
+
 
 ### connect to the DB
 try:
@@ -24,50 +32,54 @@ try:
     print ("DB connected")
 except psycopg2.Error as err:
     print("Connection to the database failed: ", err)
-    # exit()
+
+
+# countdown timer to limit open connection to db  
+def countdown(t):
+    print('Countdown is going')
+    while t: 
+        #mins, secs = divmod(t, 60) 
+        #timer = '{:02d}:{:02d}'.format(mins, secs) 
+        #print(timer, end="\r") 
+        print ('.', end="\r")
+        time.sleep(1) 
+        t -= 1
+    print('Fire in the hole!!!') 
 
 
 ### create a cursor
 cursor = connection.cursor()
 
-### execute a single query cursor.execute("query")
-### execute multy-line query cursor.executescript
-# cursor.executescript("SQL-query")
-
-### get results: fetchall, fetchone, fetchmany
-# cursor.fetchone()
-
 ###### Get the registration, owner and car info...
 vin = input("Enter VIN: ")
 
-cars = []
-cursor.execute("select vin from vehicle;")
-lics = cursor.fetchall()
-for j in range(len(lics)):
-    cars.append(lics[j][0])
-
 ### check if car number is in vehicle table, 
 ### if not, print the error message
-if vin not in cars:
+cars = []
+cursor.execute("SELECT vin FROM vehicle WHERE vin = (%s);", (vin, ))
+lics = cursor.fetchall()
+if lics[0] == '':
     print ("The vehicle is not found.")
     cursor.close()
     connection.close()
     print ("Connection closed")
 else:
-    ### get car info from the DB
-    sql_read_car = '''SELECT brand, model, year 
-                      FROM 
-                        (SELECT v.vin,
-                            b.name as brand,
-                            m.name as model,
-                            m.year
-                          FROM brand as b
-                          JOIN model as m ON b.id = m.brand_id
-                          JOIN vehicle AS v ON v.model_id = m.id) AS sub
-                    WHERE vin = (%s);'''
+    sql_read_car = '''SELECT b.name AS brand, 
+                             m.name AS model, 
+                             o.name AS owner, 
+                             r."From" AS "reg. from"
+                      FROM registration AS r
+                        JOIN vehicle AS v ON v.id = r.vehicleid
+                        JOIN brand AS b ON b.id = v.brand_id
+                        JOIN model AS m ON m.id = v.model_id
+                        JOIN owner AS o ON o.id = r.ownerid
+                      WHERE r."To" IS NULL
+                        AND v.vin = (%s); '''
+
+
     cursor.execute(sql_read_car, (vin, ))
     vehic = cursor.fetchall()[0]
-    print ("Car information: brand: {}, model: {}, year: {}".format(vehic[0], vehic[1], vehic[2]))
+    print ("Car information: \n brand: {}\n model: {}\n owner: {}\n reg. from: {}\n".format(vehic[0], vehic[1], vehic[2], vehic[3]))
     
     ### get registration info from DB
     sql_read_reg = ''' WITH cte_reg AS (
@@ -87,29 +99,27 @@ else:
     reg_info = cursor.fetchone()
     print ('''Current registration: 
               owner: {}, regnum: {}, 
-              From: {}, To: {}'''.format(reg_info[0], reg_info[1], reg_info[2], reg_info[3]) )
+              From: {}, To: now'''.format(reg_info[0], reg_info[1], reg_info[2]) )
 
 
 reg_input = input("Do you want to add new registration (Y/n)? ").lower()
 print (reg_input)
 if reg_input == 'y' or reg_input == '':
     name = input("Enter new owner's name: ")
-
-    names = []
-    cursor.execute("SELECT name FROM owner;")
-    owners = cursor.fetchall()
-    for i in range(len(owners)):
-        names.append(owners[i][0])    
-
+    #uid = input("Enter owner's ID: ")
+    
 ### check if name is in owner table, 
 ### if not, add new to the table owners, print the message
-    if name not in names:
-         sql_insert = ("INSERT INTO owner(name) VALUES (%s);")
-         cursor.execute(sql_insert, (name, ))
-         connection.commit() 
-         print ("New owner added to DB.")
+    cursor.execute("SELECT name FROM owner WHERE name = (%s) ;", (name, ))
+    owners = cursor.fetchall()
+    if owners[0] == '':
+        sql_insert = ("INSERT INTO owner(name) VALUES (%s);")
+        cursor.execute(sql_insert, (name, ))
+        connection.commit() 
+        print ("New owner added to DB.")
     else:
         pass
+        
     
     new_num = input("ENTER new registratin number (not longer than 10 characters): ")
     
@@ -130,75 +140,5 @@ if reg_input == 'y' or reg_input == '':
 else:
     print("Goodbye!")
     
-
 cursor.close()
 connection.close()     
-
-
-
-
-### get data to write
-# name = input("Enter new owner's name: ")
-
-# names = []
-# cursor.execute("SELECT name FROM owner;")
-# owners = cursor.fetchall()
-# for i in range(len(owners)):
-#     names.append(owners[i][0])
-
-# ### check if name is in owner table, 
-# ### if not, add new to the table owners, print the message
-# if name not in names:
-#     sql_insert = ("INSERT INTO owner(name) VALUES (%s);")
-#     cursor.execute(sql_insert, (name, ))
-#     connection.commit() 
-#     print ("New owner added to DB.")
-# else: 
-#     pass
-
-
-
-# sql_sel = ''' SELECT vehicleid,
-#                      registrationnum,
-#                     "From",
-#                     "To"
-#              FROM registration 
-#              JOIN vehicle AS v ON vehicleid = v.id WHERE v.licence = (%s);      
-# '''
-
-# cursor.execute(sql_sel, (vin, ))
-# regs = cursor.fetchall()
-# vid, regnum, dto, dfrom = [], [], [], []
-# for k in range(len(regs)):
-#     vid.append(regs[k][0])
-#     regnum.append(int(regs[k][1]))
-#     dto.append(regs[k][2])
-#     dfrom.append(regs[k][3])
-
-# newreg = max(regnum) + 1
-
-# sql_upd = '''UPDATE registration SET "To" = now()
-#              WHERE "To" IS NULL AND vehicleid = (%s);'''
-# cursor.execute(sql_upd, (vid[0], ))
-# connection.commit()
-# print("Previous record is updated")
-             
-
-# sql_newreg = '''INSERT INTO registration(vehicleid, ownerid, registrationnum, "From")
-#                 SELECT v.id, o.id, (%s), now()
-#                 FROM vehicle AS v, owner AS o
-#                 WHERE v.licence = (%s) AND o.name = (%s);'''
-# ins_data = (newreg, vehicle, name)
-# cursor.execute(sql_newreg, ins_data)
-# connection.commit() 
-# print ("New registration record added to DB.")    
-
-# ### print("Current owner(s): ... 
-# ### ans = input("Update record (Y/n)?")
-# ### if input == lowercase('Y') or input == '':
-# ###    do sql_upd =  '''UPDATE registration SET "To" = now()
-# ###                     WHERE "To" = NULL AND vehicleid = 1; '''
-
-
-# cursor.close()
-# connection.close()
